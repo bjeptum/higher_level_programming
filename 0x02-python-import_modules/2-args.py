8bc08fed8a0a42af69a96d1b653faf18d7a4c5fc
b_jeptum <brendajeptum@gmail.com>
2022-12-01 11:22:12 -0800

    Making a script dynamic using sys.argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    length = (len(sys.argv)-1)
+
+    if length == 0:
+        print("{} argument".format(length))
+    elif length == 1:
+        print("{} argument".format(length))
+    else:
+        print("{} arguments".format(length))
+    count = 1
+    list = sys.argv[1:]
+    for item in list:
+        print("{} : {}".format(count, item))
+        count = count + 1
