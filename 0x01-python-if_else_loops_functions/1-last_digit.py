d3bf2793b186d6d7d1fea45ee824265113761f2b
b_jeptum <brendajeptum@gmail.com>
2022-11-30 22:37:44 -0800

    Last digit of randiom numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+last_num = int(repr(number)[-1])
+if last_num < 6 and last_num != 0:
+    print(f"Last digit of {number} is {last_num} and is less than 6 but not 0")
+elif last_num == 0:
+    print(f"Last digit of {number} is {last_num} and is 0")
+else:
+    print(f"Last digit of {number} is {last_num} and is greater than 5")
