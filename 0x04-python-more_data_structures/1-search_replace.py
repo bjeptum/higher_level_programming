ef66768dd807b530ae88491de77454e5e649d9c0
b_jeptum <brendajeptum@gmail.com>
2022-12-07 07:37:40 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        if i == search:
+            my_lits.replace(i, replace)
+            print(my_list)
