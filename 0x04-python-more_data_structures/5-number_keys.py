0a43bb0d30d3d4fdc908e55c8b68283e648ad122
b_jeptum <brendajeptum@gmail.com>
2022-12-08 00:12:47 -0800

    Print sorted dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    count = 0
-    for i in a_dictionary:
-        count += 1
-        return(count)
+    sorted_by_key = dict(sorted(a_dictionary.items()))
