83ef9d74af09a97c43f60e293fc1d00f0de3b6bf
b_jeptum <brendajeptum@gmail.com>
2022-12-08 10:55:03 -0800

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     v = list(a_dictionary.values())
     k = list(a_dictionary.keys())
-    return k[v.index(max(v))]
+    best = k[v.index(max(v))]
+    if best == 0:
+        return None
+    else:
+        return best
