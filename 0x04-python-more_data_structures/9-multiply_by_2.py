761b75a188265b3c027ebecd7a15bf95cf2488ff
b_jeptum <brendajeptum@gmail.com>
2022-12-08 07:06:47 -0800

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+    new_dict = a_dictionary.copy
+    for key in new_dict:
+        new_dict[key] = new_dict[key] * 2
+    return new_dict
 
 
 def print_sorted_dictionary(a_dictionary):
