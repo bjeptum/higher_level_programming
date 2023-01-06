356262190c81b50106da9d98c722e19109c6767e
b_jeptum <brendajeptum@gmail.com>
2023-01-06 08:42:08 -0800

    Add two integers test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    ""i"
-    Adds two integers and returns the result
+    ""i"Adds two integers and returns the result
     """
     if type(a) == float or type(b) == float:
         a = int(a)
