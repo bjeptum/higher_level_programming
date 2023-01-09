1b43a3a15b244ef1e7fc7144cfdb6cef4911c347
b_jeptum <brendajeptum@gmail.com>
2023-01-09 12:12:15 -0800

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 1-my_list module
-An inherited xlass that prints a sorted list
+An inherited class
 """
 
 
 class MyList(list):
-    """Inherited Class"""
+    """MyList Class"""
     def print_sorted(self):
         """Prints a sorted list"""
         print(sorted(self))
