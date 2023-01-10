c50d725c12d8c97f70ea76c742cc12a953400b1e
b_jeptum <brendajeptum@gmail.com>
2023-01-10 11:45:18 -0800

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Full rectangle module
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle subclass"""
+    def __init__(self, width, height):
+        """Instantiation method"""
+        self.__width = width
+        self.__height = height
+        super(Rectangle, self).integer_validator('width', self.__width)
+        super(Rectangle, self).integer_validator('height', self.__height)
+        print(f"[Rectangle] {width}/{height}")
+
+    def area(self):
+        """Calculates area"""
+        return self.__width * self.__height
