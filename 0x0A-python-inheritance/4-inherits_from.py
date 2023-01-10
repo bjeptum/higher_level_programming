b91dfdac2883f6fa203c23cec103f4ae98f99c81
b_jeptum <brendajeptum@gmail.com>
2023-01-10 04:04:25 -0800

    Only subclass of updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Checks if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class
     """
-    return(isinstance(obj, a_class))
+    return(issubclass(type(obj), a_class))
