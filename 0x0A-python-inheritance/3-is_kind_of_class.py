856362b6f7e5b4707548d6a6dde9300cb72cad4c
b_jeptum <brendajeptum@gmail.com>
2023-01-10 01:07:56 -0800

    Is of same class or inherited from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if the  object is an instance of the inherited/specified class"""
+    return(isinstance(obj, a_class))
