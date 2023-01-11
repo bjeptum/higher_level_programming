bfc2a2a14b09b4a8f30b96e907d18eea90424ace
b_jeptum <brendajeptum@gmail.com>
2023-01-11 12:23:03 -0800

    Full rectangle updated

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,8 +14,11 @@ class Rectangle(BaseGeometry):
         self.__height = height
         super(Rectangle, self).integer_validator('width', self.__width)
         super(Rectangle, self).integer_validator('height', self.__height)
-        print(f"[Rectangle] {width}/{height}")
 
     def area(self):
         """Calculates area"""
         return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a rectangle description"""
+        return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
