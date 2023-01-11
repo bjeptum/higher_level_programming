8311afaa62845e598605ce841f1879ed0fc3bc25
b_jeptum <brendajeptum@gmail.com>
2023-01-11 04:53:27 -0800

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+10-square module
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass of Rectangle"""
+    def __init__(self, size):
+        """Instantiation method"""
+        self.__size = size
+        super().__init__(size, size)
+
+        def area(self):
+            """Calculates area"""
+            return self.__size ** 2
