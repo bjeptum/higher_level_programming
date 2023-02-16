ac80f6ea1f110834e517a1ec3090512c9c0bf767
b_jeptum <brendajeptum@gmail.com>
2023-02-16 12:38:37 -0800

    Second commit list duplicates

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
 -- Lists records with the same score --
-SELECT score,
-COUNT(score) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-HAVING COUNT(score) > 1;
+ORDER BY number DESC
