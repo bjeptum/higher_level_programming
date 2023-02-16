cb91ab043afc37592fc631617b2e9fc1509baf46
b_jeptum <brendajeptum@gmail.com>
2023-02-16 14:44:02 -0800

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates database adn table with id as PK --
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INTEGER NOT NULL UNIQUE,
-    name VARCHAR(256)  NOT NULL
+    `id`  INTEGER NOT NULL UNIQUE,
+    `name`  VARCHAR(256)  NOT NULL
     PRIMARY KEY (id));
