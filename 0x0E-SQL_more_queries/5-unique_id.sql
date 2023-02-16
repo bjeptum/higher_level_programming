c96c3506ff8bd52d24da4dd7d3613203129fe435
b_jeptum <brendajeptum@gmail.com>
2023-02-16 14:30:03 -0800

    Second commit Create table with unique default id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,6 @@
 -- Creates the table unique_id --
 CREATE TABLE IF NOT EXISTS unique_id(
-    DISTINCT id INTEGER DEFAULT 1,
+    id INTEGER DEFAULT 1 UNIQUE,
     name VARCHAR(256));
+    INSERT IGNORE INTO unique_id (id, name)
+    VALUES (1, 'Bob');
