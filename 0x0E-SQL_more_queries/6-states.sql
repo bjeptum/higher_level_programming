5bf3cfe0f4fcac58b73ccd76f42a56c8bd16f39e
b_jeptum <brendajeptum@gmail.com>
2023-02-17 07:48:12 -0800

    Second commit States Table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
--- Creates database adn table with id as PK --
+-- Creates database andi table with id as PK --
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-    `id`  INTEGER NOT NULL UNIQUE,
-    `name`  VARCHAR(256)  NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id  INTEGER NOT NULL AUTO_INCREMENT,
+    name  VARCHAR(256)  NOT NULL,
     PRIMARY KEY (id));
