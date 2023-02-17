648cee62a8d6e1dc5aa7eeffbfb46978bc1c88b1
b_jeptum <brendajeptum@gmail.com>
2023-02-17 04:35:21 -0800

    Second commit average temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Imports table and displays average city's temperature in descending order--
-SELECT city , AVG(value) AS average_temp
+SELECT city , AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average_temp DESC;
+ORDER BY avg_temp DESC;
