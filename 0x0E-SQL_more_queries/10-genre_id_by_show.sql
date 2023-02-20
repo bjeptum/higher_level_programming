ce97a3ca41912409cc6b82f57d5718f22533d90f
b_jeptum <brendajeptum@gmail.com>
2023-02-19 23:19:05 -0800

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained that have at least one genre linked --
-SELECT t.title, g.genre_id
+SELECT t.tittle, g.genre_id
   FROM tv_shows AS t
         INNER JOIN tv_show_genres AS g
 	ON t.id = g.show_id
