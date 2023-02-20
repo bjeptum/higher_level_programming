e3e7ecebdfe12b16cc2ed4512b9d83d60c11365e
b_jeptum <brendajeptum@gmail.com>
2023-02-19 22:58:01 -0800

    Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all cities in the databses with their states --
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
