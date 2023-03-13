a1d0f2453a867430fed97e2fb789cb55097f28ae
b_jeptum <brendajeptum@gmail.com>
2023-03-13 10:50:53 -0700

    Updated print first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 // Prints the first argument passed to it
+// Import process module
 const process = require('process');
-const args = process.argv.slice(2);
-if (args) {
-  console.log(args);
-} else {
+const args = process.argv;
+let len = 0;
+
+// Calculate length of argv array
+args.forEach((item) => {
+  len++;
+});
+
+if (len <= 2) {
   console.log('No argument');
+} else {
+  console.log(args[2]);
 }
