3b710a9cec347a1c1bd9c24a178f518235ea9ed1
b_jeptum <brendajeptum@gmail.com>
2023-03-13 06:11:09 -0700

    Semistandard compliant file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-//Prints a string "JavaScript is amazing”
-message ="JavaScript is amazing"
-
-function msg(){
-	console.log(message);
+// Prints a string "JavaScript is amazing”
+function msg () {
+  const message = 'JavaScript is amazing';
+  console.log(message);
 }
 msg();
