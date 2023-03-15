ae7b63ca17c13f3f5aac126adc3a14aed8a9154c
b_jeptum <brendajeptum@gmail.com>
2023-03-15 11:53:12 -0700

    Fourth rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+/*
+ Defines a Rectangle
+ If w or h is equal to 0 or not a positive integer
+ create an empty object
+ print(): character X used to print rectangle
+ rotate(): rotates width and height
+ double(): multiplies width and height by 2
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
