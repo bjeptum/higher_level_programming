a5d787646797490c9bbdb70f7ea2e8ce916fd68b
b_jeptum <brendajeptum@gmail.com>
2023-03-15 07:02:36 -0700

    Rectangle 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,11 +6,9 @@
 */
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
+	  if (w > 0 && h > 0) {
+		  this.width = w;
+		  this.height = h;
   }
 }
 module.exports = Rectangle;
