fbd40a63d0fe2119db6f86b79bdafb01552e1ceb
b_jeptum <brendajeptum@gmail.com>
2023-03-15 14:58:28 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -6,7 +6,9 @@
 */
 const list = require('./100-data').list;
 
-const newList = list.map((value, index) => value * index);
+const newList = list.map((value, index) => {
+  return (value * index);
+});
 
 console.log('Initial list: ', list);
 console.log('New list: ', newList);
