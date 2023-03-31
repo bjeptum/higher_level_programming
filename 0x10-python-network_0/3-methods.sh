277f74aced45366f8c424b64d404034f1341ce1c
root <brendajeptum@gmail.com>
2023-03-31 08:57:02 -0700

    Improved3 cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep "Allow: " | awk '{$1=""; print $0}'
+curl -sI -X OPTIONS "$1" | grep "Allow: " | awk '{ $1=""; print $0 }'
