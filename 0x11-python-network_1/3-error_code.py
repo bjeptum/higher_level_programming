490ea1e6744b917e010a6468e641cdec68fb63c3
b_jeptum <brendajeptum@gmail.com>
2023-04-04 11:40:40 -0700

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL
+Then displays body of response (decoded in utf-8)
+"""
+
+import urllib.request
+from urllib.request import urlopen
+from urllib.error import URLError
+import sys
+
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as r:
+            body = response.read()
+            body_content = body.decode('utf-8')
+            print(body_content)
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
