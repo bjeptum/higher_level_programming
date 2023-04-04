feb10cb834f2af471df6a59bd0974488c12bf295
b_jeptum <brendajeptum@gmail.com>
2023-04-04 11:18:48 -0700

    POST an email#0_2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,13 +9,14 @@ then displays the body of the response
 import urllib.request
 from urllib.request import urlopen
 import sys
+import urllib.parse
 
 
 if __name__ == '__main__':
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = email.encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode()
     with urllib.request.urlopen(url, data) as r:
         response_data = r.read().decode('utf-8')
         print(response_data)
