39d72e2c9807d19b6997bdc92d749d8ff7e81f36
b_jeptum <brendajeptum@gmail.com>
2023-04-14 00:28:23 -0700

    Prints string in uppercase #2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,10 +5,13 @@ Not allowed to import any module
 No use of str.upper() and str.isupper()
 """
 
+
 def uppercase(str):
     uppercase_str = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
             new_c = chr(ord(char) - 32)
             uppercase_str += new_c
-    print("{}\n".format(uppercase_str))
+        else:
+            uppercase_str += char
+    print("{}\n".format(uppercase_str), end='')
