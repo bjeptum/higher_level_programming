feed7b455c70fb6649d9292d86d20510853205e3
b_jeptum <brendajeptum@gmail.com>
2023-04-14 05:24:14 -0700

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Prints the numbers from 1 to 100 separated by a space
+"""
+
+
+def fizzbuzz():
+    for num in range(1, 100):
+        if (num % 15) == 0:
+            print("FizzBuzz", end=" ")
+        elif (num % 3) == 0:
+            print("Fizz", end=" ")
+        elif (num % 5) == 0:
+            print("Buzz", end=" ")
+        else:
+            print(f"{num:d}", end=" ")
