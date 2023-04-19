fa92b453a42fd9cfe6eadf769aa24cc4d7e44ad3
b_jeptum <111267046+bjeptum@users.noreply.github.com>
2023-04-19 10:32:50 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// Prints a string "JavaScript is amazing”
-function msg () {
-  const message = 'JavaScript is amazing';
-  console.log(message);
-}
-msg();
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// Prints  3 lines using array and loop
-const msg = [];
-msg.push('C is fun', 'Python is cool', 'JavaScript is amazing');
-const len = msg.length;
-for (let i = 0; i < len; i++) {
-  console.log(msg[i]);
-}
