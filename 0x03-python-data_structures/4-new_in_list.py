ffa28b7807107da4f19b54f4c13f7ecc97cfcd1e
root <brendajeptum@gmail.com>
2023-04-26 01:14:30 -0700

    Replace an element in a list without altering the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx < 0 or idx >= length:
+        return my_list.copy()
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
