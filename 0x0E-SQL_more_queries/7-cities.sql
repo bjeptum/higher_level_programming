b1480f5edbcfef7d0cd42368e4032747d7f6dde4
root <brendajeptum@gmail.com>
2023-05-17 11:45:10 -0700

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- Create table on database with specific details --
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INTEGER NOT NULL UNIQUE  AUTO_INCREMENT,
+    id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
     state_id INTEGER NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id));
