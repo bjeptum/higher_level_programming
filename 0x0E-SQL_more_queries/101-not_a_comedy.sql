130de6f3ee5e2c6c195604f1739af8143a2b66a6
root <brendajeptum@gmail.com>
2023-05-19 00:55:04 -0700

    Shows without the genre Comedy#3wq

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,9 @@
 -- Retrieves all shows without the genre Comedy --
 SELECT tv_shows.title FROM tv_shows
 WHERE tv_shows.id NOT IN (
-    SELECT tv_genres.tv_show_id
-    FROM tv_shows.tv_genres
-    JOIN tv_shows.genres ON genres.id = tv_genres.genre_id
-    WHERE tv_genres.name = "Comedy")
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres_id
+    WHERE tv_genres.name = "Comedy"
+)
 ORDER BY tv_shows.title ASC;
