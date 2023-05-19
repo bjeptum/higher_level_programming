6d553f60f697758c89fe4033cf5341d9d5d9d0ae
root <brendajeptum@gmail.com>
2023-05-19 00:38:40 -0700

    Create user and grant all privileges#2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates user with all privileges --
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%';
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
 WITH GRANT OPTION;
