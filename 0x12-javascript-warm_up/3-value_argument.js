ff2915c7127e3de171fc44888ec338bd2e6e98c0
root <brendajeptum@gmail.com>
2023-06-12 12:00:42 -0700

    Prints the first argument passed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Prints first argument passed
+const process = require('process');
+const myArgs = process.argv;
+let len = 0;
+myArgs.forEach((item) => {
+	len++;
+});
+if (len <= 2) {
+	console.log('No argument');
+} else {
+	console.log(myArgs[2]);
+}
