54b402a80de4685dbff3845f41bb1e9ac9e50ac0
root <brendajeptum@gmail.com>
2023-06-12 12:58:08 -0700

    Prints 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Print three strings
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = myVar.length;
+let i = 0;
+while (i < len) {
+  console.log(myVar[i]);
+  i++;
+}
