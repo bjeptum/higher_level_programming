57c6e33c56f414a0ff255b7d44ee643151521e4f
root <brendajeptum@gmail.com>
2023-06-13 08:18:52 -0700

    Function to add integers

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Returns the addition of 2 integers
+Function must be visible from outside
+*/
+exports.add = function add (a, b) {
+  const addition = a + b;
+  return (addition);
+};
