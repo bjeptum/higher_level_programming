35b9c208cb1a71c259432bf328cdd661ed0bcd27
root <brendajeptum@gmail.com>
2023-06-13 03:30:57 -0700

    Function to add integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints the addition of 2 integers
+// Prints x times C is fun
+const process = require('process');
+const args = process.argv;
+const n1 = parseInt(args[2]);
+const n2 = parseInt(args[3]);
+function add (a, b) {
+  const addition = a + b;
+  return (addition);
+}
+console.log(add(n1, n2));
