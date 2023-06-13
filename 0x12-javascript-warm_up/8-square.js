47699157949e8366b22fded97f92d759102119d3
root <brendajeptum@gmail.com>
2023-06-13 02:39:32 -0700

    Print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Prints a square
+const process = require('process');
+const args = process.argv;
+const x = parseInt(args[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let str = '';
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      str += 'X';
+    }
+    if (i !== x - 1) {
+      str += '\n';
+    }
+  }
+  console.log(str);
+}
