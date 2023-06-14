72148dd84dd344c474de52e9ccd9c3383347d874
root <brendajeptum@gmail.com>
2023-06-14 14:46:11 -0700

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,12 +2,15 @@
 /*
 Concats files
 */
-const process = require('process');
+const fs = require('fs');
 const myArgs = process.argv;
 const arg1 = myArgs[2];
 const arg2 = myArgs[3];
 const arg3 = myArgs[4];
 
-const { FilesManager } = require('turbodepot-node');
-let filesManager = new FilesManager();
-filesManager.mergeFiles(['arg1', 'arg2'], 'arg3', "\n");
+const File1 = fs.readFileSync(arg1, 'utf8');
+const File2 = fs.readFileSync(arg2, 'utf8');
+
+const mergeFiles = File1 + File2;
+
+fs.writeFileSync(arg3, mergeFiles);
