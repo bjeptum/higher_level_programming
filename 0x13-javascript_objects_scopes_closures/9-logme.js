89bac9090ce6b5e78461a4770870fc83eb9dc6c2
root <brendajeptum@gmail.com>
2023-06-14 12:43:37 -0700

    Count number of arguments printed and new argument value

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Counts the number of arguments printed and new argument value
+*/
+let count = 0;
+exports.logMe = function (item) {
+	console.log(count + ':' + item);
+	count++;
+};	
