84d0f2cf5388ba1abe3fc1e9eed49d6a8b7a5224
root <brendajeptum@gmail.com>
2023-06-17 11:37:01 -0700

    List values of state name as per user input and safe from MySQL Injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Displays values of the state entered as argument
+Handle SQL Injection
+From database hbtn_0e_0_usa
+Ordered by states.id
+Used the module MySQLdb
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # Connecting to hbtn_0e_0_usa db
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
+    # Create cursor
+    curx = db.cursor()
+    # Query data as per user input
+    u_input = argv[4]
+    query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    curx.execute(query, (u_input,))
+    rows = curx.fetchall()
+    for row in rows:
+        print(row)
+        # Close  cursor and db
+    curx.close()
+    db.close()
