6759310f04c5e48e29c81dfc33258183d505e8b5
root <brendajeptum@gmail.com>
2023-06-18 23:01:11 -0700

    Filter sttaes containing letter a in Dattabase using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Lists all states that contain the letter a
+"""
+
+from sys import argv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine, MetaData
+from sqlalchemy.engine import result
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+
+    # Connect to MySQL server running on localhost
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Create engine and connect to db
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+                             {password}@localhost:3306/{database}')
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query database and print results
+    results = session.query(State).filter(State.name.like('%a%'))
+
+    for state in results:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
