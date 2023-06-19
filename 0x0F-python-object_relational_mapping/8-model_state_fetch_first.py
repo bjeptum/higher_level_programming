9a48e4ea386be98fed177522667ea9e1665e92cd
root <brendajeptum@gmail.com>
2023-06-18 19:55:26 -0700

    Prints first state object in Dattabase using SQLQAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,35 @@
+#!/usr/biin/python3
+"""
+Returns the first state object from database
+"""
+
+from sys import argv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine, MetaData
+from sqlalchemy.engine import result
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+
+    # Connect to MySQL server running on localhost
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Create engine and connect to db
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+                             {password}@localhost:3306/{database}')
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query database and print results
+    state_1 = session.query(State).first()
+
+    if state_1 is None:
+        print("Nothing")
+    else:
+        print(f"{state_1.id}: {state_1.name}")
+
+    session.close()
