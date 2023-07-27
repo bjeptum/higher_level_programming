4773e91f7db9a3c02d6cb10e6ee88bf913e04f86
root <brendajeptum@gmail.com>
2023-07-27 13:46:54 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/*
+Prints the number of movies
+where the character “Wedge Antilles” is present
+*/
+const request = require('request');
+const url = process.argv[2];
+const characterId = 18;
+const count = 0;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  if (response.statusCode === 200) {
+    // Parse the response body as JSON
+    const filmsData = JSON.parse(body).results;
+
+    // Check for  Wedge Antilles
+	 for ( const film of filmsData) {
+		 const characters = film.characters;
+		 for ( const character in characters) {
+			 if ( character.includes(characterId + '/')) {
+				 count = count + 1;
+			 }
+		 }
+	 }
+	  console.log(count);
+  }
+});
