da9a5557616ef49762c0cb71a24d9b7c37548f97
root <brendajeptum@gmail.com>
2023-07-27 11:11:19 -0700

    Scripts that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,10 +5,10 @@ Reads and prints the content of a file
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function(err,data) {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
