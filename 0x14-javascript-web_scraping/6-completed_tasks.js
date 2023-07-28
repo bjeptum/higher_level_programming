b503f188a86d65e4a0d0492315fb031a4b3f3cba
root <brendajeptum@gmail.com>
2023-07-28 03:28:48 -0700

    Output all the completed tasks by employees

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+/*
+Computes the number of tasks completed by user id
+*/
+const request = require('request');
+
+function getUsersWithCompletedTasks (url) {
+  // Get all todo tasks from the API
+  request.get(url, (error, response, todosData) => {
+    if (error) {
+      console.error('Error fetching todo list:', error);
+      return;
+    }
+
+    const todos = JSON.parse(todosData);
+    const usersCompletedTasks = {};
+
+    // Count completed tasks for each user ID
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (!usersCompletedTasks[todo.userId]) {
+          usersCompletedTasks[todo.userId] = 1;
+        } else {
+          usersCompletedTasks[todo.userId]++;
+        }
+      }
+    });
+
+    console.log(usersCompletedTasks);
+  });
+}
+
+if (process.argv.length !== 3) {
+  console.log('Usage: script_name.js url');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+getUsersWithCompletedTasks(url);
