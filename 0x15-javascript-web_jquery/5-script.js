784c2d6573fe5423fb51882d702ff16d21fb689a
b_jeptum <brendajeptum@gmail.com>
2023-08-07 23:30:46 +0300

    List of elements

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,6 +1,6 @@
 //Adds a <li> element to a list when the user clicks on the tag
 $(document).ready(function() {
-  $("add_item").click(function() {
+  $("#add_item").click(function() {
     var newItem = $('<li>Item</li>');
     $('ul.my_list').append(newItem);
   });
