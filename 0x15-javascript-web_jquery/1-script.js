facd65b451134d0f6a216cacf34e459d9e5edcd1
b_jeptum <brendajeptum@gmail.com>
2023-08-07 22:13:42 +0300

    Use jquery to modify element

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// Select element use queryselector with jquery
+$(document).ready(function() {
+    //Select the header element
+    const header = $('header');
+    //Modify the color of the header
+    header.css('color', '#FF0000');
+});
