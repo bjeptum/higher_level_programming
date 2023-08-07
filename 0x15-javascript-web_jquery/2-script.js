0b60153653659cf6f74360dc3429b0a4b0edf2fa
b_jeptum <brendajeptum@gmail.com>
2023-08-07 22:31:12 +0300

    Updates the text color of the <header> element to red when user clicks on the tag DIV#red_header

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// updates text color when user clicks on the tag
+$(document).ready(function() {
+    $("#red_header").click(function() {
+        $('header').css('color', '#FF0000');
+    });
+});
